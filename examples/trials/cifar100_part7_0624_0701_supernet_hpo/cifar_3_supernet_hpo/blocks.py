e646c9fc95b5ba2f2235dea5bdd6fbea9499ccdf
aptsunny <aptsunny@tongji.edu.cn>
2020-06-30 11:36:53 +0000

    0630_best_hyper+layer-wise

---
+++ examples/trials/cifar100_part7_0624_0701_supernet_hpo/cifar_3_supernet_hpo/blocks.py
@@ -0,0 +1,178 @@
+import torch
+import torch.nn as nn
+
+class hsigmoid(nn.Module):
+    def forward(self, x):
+        out = F.relu6(x + 3, inplace=True) / 6
+        return out
+
+class SeModule(nn.Module):
+    def __init__(self, in_size, reduction=4):
+        super(SeModule, self).__init__()
+        self.se = nn.Sequential(
+            nn.AdaptiveAvgPool2d(1),
+            nn.Conv2d(in_size, in_size // reduction, kernel_size=1, stride=1, padding=0, bias=False),
+            nn.BatchNorm2d(in_size // reduction),
+            nn.ReLU(inplace=True),
+            nn.Conv2d(in_size // reduction, in_size, kernel_size=1, stride=1, padding=0, bias=False),
+            nn.BatchNorm2d(in_size),
+            hsigmoid()
+        )
+    def forward(self, x):
+        return x * self.se(x)
+
+class Inverted_Bottleneck(nn.Module):
+    '''expand + depthwise + pointwise'''
+    def __init__(self, kernel_size, in_size, expand_size, out_size, nolinear, semodule, stride):
+        super(Inverted_Bottleneck, self).__init__()
+        self.stride = stride
+        self.se = semodule
+
+        self.conv1 = nn.Conv2d(in_size, expand_size, kernel_size=1, stride=1, padding=0, bias=False)
+        self.bn1 = nn.BatchNorm2d(expand_size)
+        self.nolinear1 = nolinear
+        self.conv2 = nn.Conv2d(expand_size, expand_size, kernel_size=kernel_size, stride=stride, padding=kernel_size//2, groups=expand_size, bias=False)
+        self.bn2 = nn.BatchNorm2d(expand_size)
+        self.nolinear2 = nolinear
+        self.conv3 = nn.Conv2d(expand_size, out_size, kernel_size=1, stride=1, padding=0, bias=False)
+        self.bn3 = nn.BatchNorm2d(out_size)
+
+        self.shortcut = nn.Sequential()
+        if stride == 1 and in_size != out_size:
+            self.shortcut = nn.Sequential(
+                nn.Conv2d(in_size, out_size, kernel_size=1, stride=1, padding=0, bias=False),
+                nn.BatchNorm2d(out_size),
+            )
+
+    def forward(self, x):
+        out = self.nolinear1(self.bn1(self.conv1(x)))
+        out = self.nolinear2(self.bn2(self.conv2(out)))
+        out = self.bn3(self.conv3(out))
+        if self.se != None:
+            out = self.se(out)
+        out = out + self.shortcut(x) if self.stride==1 else out
+        return out
+
+class Shufflenet(nn.Module):
+    def __init__(self, inp, oup, mid_channels, *, ksize, stride):
+        super(Shufflenet, self).__init__()
+        self.stride = stride
+        assert stride in [1, 2]
+        assert ksize in [3, 5, 7]
+
+        self.base_mid_channel = mid_channels
+        self.ksize = ksize
+        pad = ksize // 2
+        self.pad = pad
+        self.inp = inp
+
+        outputs = oup - inp
+
+        branch_main = [
+            # pw
+            nn.Conv2d(inp, mid_channels, 1, 1, 0, bias=False),
+            nn.BatchNorm2d(mid_channels, affine=False),
+            nn.ReLU(inplace=True),
+            # dw
+            nn.Conv2d(mid_channels, mid_channels, ksize,
+                      stride, pad, groups=mid_channels, bias=False),
+            nn.BatchNorm2d(mid_channels, affine=False),
+            # pw-linear
+            nn.Conv2d(mid_channels, outputs, 1, 1, 0, bias=False),
+            nn.BatchNorm2d(outputs, affine=False),
+            nn.ReLU(inplace=True),
+        ]
+        self.branch_main = nn.Sequential(*branch_main)
+
+        if stride == 2:
+            branch_proj = [
+                # dw
+                nn.Conv2d(
+                    inp, inp, ksize, stride, pad, groups=inp, bias=False),
+                nn.BatchNorm2d(inp, affine=False),
+                # pw-linear
+                nn.Conv2d(inp, inp, 1, 1, 0, bias=False),
+                nn.BatchNorm2d(inp, affine=False),
+                nn.ReLU(inplace=True),
+            ]
+            self.branch_proj = nn.Sequential(*branch_proj)
+
+    def forward(self, old_x):
+        if self.stride == 1:
+            x_proj, x = channel_shuffle(old_x)
+            return torch.cat((x_proj, self.branch_main(x)), 1)
+        elif self.stride == 2:
+            x_proj = old_x
+            x = old_x
+            return torch.cat((self.branch_proj(x_proj), self.branch_main(x)), 1)
+
+class Shuffle_Xception(nn.Module):
+
+    def __init__(self, inp, oup, mid_channels, *, stride):
+        super(Shuffle_Xception, self).__init__()
+
+        assert stride in [1, 2]
+
+        self.base_mid_channel = mid_channels
+        self.stride = stride
+        self.ksize = 3
+        self.pad = 1
+        self.inp = inp
+        outputs = oup - inp
+
+        branch_main = [
+            # dw
+            nn.Conv2d(inp, inp, 3, stride, 1, groups=inp, bias=False),
+            nn.BatchNorm2d(inp, affine=False),
+            # pw
+            nn.Conv2d(inp, mid_channels, 1, 1, 0, bias=False),
+            nn.BatchNorm2d(mid_channels, affine=False),
+            nn.ReLU(inplace=True),
+            # dw
+            nn.Conv2d(mid_channels, mid_channels, 3,
+                      1, 1, groups=mid_channels, bias=False),
+            nn.BatchNorm2d(mid_channels, affine=False),
+            # pw
+            nn.Conv2d(mid_channels, mid_channels, 1, 1, 0, bias=False),
+            nn.BatchNorm2d(mid_channels, affine=False),
+            nn.ReLU(inplace=True),
+            # dw
+            nn.Conv2d(mid_channels, mid_channels, 3,
+                      1, 1, groups=mid_channels, bias=False),
+            nn.BatchNorm2d(mid_channels, affine=False),
+            # pw
+            nn.Conv2d(mid_channels, outputs, 1, 1, 0, bias=False),
+            nn.BatchNorm2d(outputs, affine=False),
+            nn.ReLU(inplace=True),
+        ]
+
+        self.branch_main = nn.Sequential(*branch_main)
+
+        if self.stride == 2:
+            branch_proj = [
+                # dw
+                nn.Conv2d(inp, inp, 3, stride, 1, groups=inp, bias=False),
+                nn.BatchNorm2d(inp, affine=False),
+                # pw-linear
+                nn.Conv2d(inp, inp, 1, 1, 0, bias=False),
+                nn.BatchNorm2d(inp, affine=False),
+                nn.ReLU(inplace=True),
+            ]
+            self.branch_proj = nn.Sequential(*branch_proj)
+
+    def forward(self, old_x):
+        if self.stride == 1:
+            x_proj, x = channel_shuffle(old_x)
+            return torch.cat((x_proj, self.branch_main(x)), 1)
+        elif self.stride == 2:
+            x_proj = old_x
+            x = old_x
+            return torch.cat((self.branch_proj(x_proj), self.branch_main(x)), 1)
+
+def channel_shuffle(x):
+    batchsize, num_channels, height, width = x.data.size()
+    assert (num_channels % 4 == 0)
+    x = x.reshape(batchsize * num_channels // 2, 2, height * width)
+    x = x.permute(1, 0, 2)
+    x = x.reshape(2, -1, num_channels // 2, height, width)
+    return x[0], x[1]
---
+++ examples/trials/cifar100_part7_0624_0701_supernet_hpo/cifar_3_supernet_hpo/network.py
@@ -0,0 +1,162 @@
+import torch
+import torch.nn as nn
+from blocks import Shufflenet, Shuffle_Xception
+
+
+class ShuffleNetV2_OneShot(nn.Module):
+
+    def __init__(self, input_size=224, n_class=1000):
+        super(ShuffleNetV2_OneShot, self).__init__()
+
+        assert input_size % 32 == 0
+
+        self.stage_repeats = [4, 4, 8, 4]
+        self.stage_out_channels = [-1, 16, 64, 160, 320, 640, 1024]
+
+        # building first layer
+        input_channel = self.stage_out_channels[1]
+        self.first_conv = nn.Sequential(
+            nn.Conv2d(3, input_channel, 3, 2, 1, bias=False),
+            nn.BatchNorm2d(input_channel, affine=False),
+            nn.ReLU(inplace=True),
+        )
+
+        self.features = torch.nn.ModuleList()
+        archIndex = 0
+        for idxstage in range(len(self.stage_repeats)):
+            numrepeat = self.stage_repeats[idxstage]
+            output_channel = self.stage_out_channels[idxstage + 2]
+
+            for i in range(numrepeat):
+                if i == 0:
+                    inp, outp, stride = input_channel, output_channel, 2
+                else:
+                    inp, outp, stride = input_channel // 2, output_channel, 1
+
+                base_mid_channels = outp // 2
+                mid_channels = int(base_mid_channels)
+                archIndex += 1
+                self.features.append(torch.nn.ModuleList())
+                for blockIndex in range(4):
+                    if blockIndex == 0:
+                        # print('Shuffle3x3')
+                        self.features[-1].append(
+                            Shufflenet(inp, outp, mid_channels=mid_channels, ksize=3, stride=stride))
+                    elif blockIndex == 1:
+                        # print('Shuffle5x5')
+                        self.features[-1].append(
+                            Shufflenet(inp, outp, mid_channels=mid_channels, ksize=5, stride=stride))
+                    elif blockIndex == 2:
+                        # print('Shuffle7x7')
+                        self.features[-1].append(
+                            Shufflenet(inp, outp, mid_channels=mid_channels, ksize=7, stride=stride))
+                    elif blockIndex == 3:
+                        # print('Xception')
+                        self.features[-1].append(
+                            Shuffle_Xception(inp, outp, mid_channels=mid_channels, stride=stride))
+                    else:
+                        raise NotImplementedError
+                input_channel = output_channel
+
+        self.archLen = archIndex
+        # self.features = nn.Sequential(*self.features)
+
+        self.conv_last = nn.Sequential(
+            nn.Conv2d(
+                input_channel, self.stage_out_channels[
+                    -1], 1, 1, 0, bias=False),
+            nn.BatchNorm2d(self.stage_out_channels[-1], affine=False),
+            nn.ReLU(inplace=True),
+        )
+        self.globalpool = nn.AvgPool2d(7)
+        self.dropout = nn.Dropout(0.1)
+        self.classifier = nn.Sequential(
+            nn.Linear(self.stage_out_channels[-1], n_class, bias=False))
+        self._initialize_weights()
+
+    def forward(self, x, architecture=False):
+
+        # architecture = [0, 0, 3, 1, 1, 1, 0, 0, 2, 0, 2, 1, 1, 0, 2, 0, 2, 1, 3, 2]
+        assert self.archLen == len(architecture)
+
+        x = self.first_conv(x)
+
+        for archs, arch_id in zip(self.features, architecture):
+            x = archs[arch_id](x)
+
+        x = self.conv_last(x)
+
+        x = self.globalpool(x)
+
+        x = self.dropout(x)
+        x = x.contiguous().view(-1, self.stage_out_channels[-1])
+        x = self.classifier(x)
+        return x
+
+    def _initialize_weights(self):
+        for name, m in self.named_modules():
+            if isinstance(m, nn.Conv2d):
+                if 'first' in name:
+                    nn.init.normal_(m.weight, 0, 0.01)
+                else:
+                    nn.init.normal_(m.weight, 0, 1.0 / m.weight.shape[1])
+                if m.bias is not None:
+                    nn.init.constant_(m.bias, 0)
+            elif isinstance(m, nn.BatchNorm2d):
+                if m.weight is not None:
+                    nn.init.constant_(m.weight, 1)
+                if m.bias is not None:
+                    nn.init.constant_(m.bias, 0.0001)
+                nn.init.constant_(m.running_mean, 0)
+            elif isinstance(m, nn.BatchNorm1d):
+                nn.init.constant_(m.weight, 1)
+                if m.bias is not None:
+                    nn.init.constant_(m.bias, 0.0001)
+                nn.init.constant_(m.running_mean, 0)
+            elif isinstance(m, nn.Linear):
+                nn.init.normal_(m.weight, 0, 0.01)
+                if m.bias is not None:
+                    nn.init.constant_(m.bias, 0)
+
+if __name__ == "__main__":
+    architecture = [0, 0, 3, 1, 1, 1, 0, 0, 2, 0, 2, 1, 1, 0, 2, 0, 2, 1, 3, 2]
+    # scale_list = [0.2, 0.4, 0.6, 0.8, 1.0, 1.2, 1.4, 1.6]
+    # scale_ids = [6, 5, 3, 5, 2, 6, 3, 4, 2, 5, 7, 5, 4, 6, 7, 4, 4, 5, 4, 3]
+    # channels_scales = []
+    # for i in range(len(scale_ids)):
+    #     channels_scales.append(scale_list[scale_ids[i]])
+
+    # ------
+    import torch
+    from torchvision import models
+    from torchsummary import summary
+    from ptflops import get_model_complexity_info
+
+    # inputs = torch.randn(3, 32, 32).unsqueeze(0)
+    device = torch.device('cuda' if torch.cuda.is_available() else 'cpu')
+
+
+    model = ShuffleNetV2_OneShot(n_class=100)
+    print(model)
+
+    test_data = torch.rand(5, 3, 224, 224)
+    test_outputs = model(test_data, architecture)
+    print(test_outputs.size())
+
+    model = model.to(device)
+
+    input_size = 224 # 224 224 #
+
+    # part1
+    with  torch.cuda.device(0):
+        # 把choice拿到forward里
+        # choice = {
+        #     0: {'conv_0': [0], 'conv_1': [0], 'conv_2': [0], 'rate': 0},
+        #     2: {'conv_0': [0], 'conv_1': [0], 'conv_2': [0], 'rate': 0}}
+
+        # flops, params = get_model_complexity_info(model, (3, 32, 32), as_strings=True, print_per_layer_stat=True)
+        flops, params = get_model_complexity_info(model, (3, input_size, input_size), as_strings=True, print_per_layer_stat=True)
+        # part2
+        summary(model, (3, input_size, input_size))
+        print('{:<30}  {:<8}'.format('Computational complexity: ', flops))
+        print('{:<30}  {:<8}'.format('Number of parameters: ', params))
